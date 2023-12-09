6d1fbe712eb8b3d4adae32cdeda673bc0864dfd9
HodgeyHodge <christopher@hodgey.co.uk>
2023-12-09 12:03:25 +0000

    Day eight part 2

--- 8/solution.py
+++ 8/solution.py
@@ -1,4 +1,6 @@
 
+from math import gcd
+
 def load_data(filename):
     with open(filename, 'r') as file:
         directions = file.readline().strip("\n")
@@ -17,7 +19,36 @@ def part_one(filename):
             print(f"{filename}: {index}")
             break
 
+def lowest_common_multiple(l):
+    output = 1
+    for e in l:
+        output *= e // gcd(output, e)
+    return output
+
+def part_two(filename):
+    directions, nodes = load_data(filename)
+    for node in [node for node in nodes if node[2] == "A"]:
+        index = 0
+        print(f"Inspecting node {node}")
+        for i in range(0, 99999):
+            node = nodes[node][0 if directions[index % len(directions)] == "L" else 1]
+            index += 1
+            if node[2] == "Z":
+                print(f"Reached node {node} at index {index}")
+
 part_one("testinput1.txt")
 part_one("testinput2.txt")
 part_one("input.txt")
 
+part_two("input.txt")
+
+# See a repeating pattern for each node with the following periods:
+#     MXA: 16343
+#     VQA: 11911
+#     CBA: 20221
+#     JBA: 21883
+#     AAA: 13019
+#     HSA: 19667
+# So answer is LCM of these periods:
+
+print(lowest_common_multiple([16343, 11911, 20221, 21883, 13019, 19667]))
